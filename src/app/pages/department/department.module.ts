c7f0bed88ec00464658747dab22891d7dffd8350
Yasiru Sellahewa <yasirusellahewa@gmail.com>
2020-10-11 14:20:43 +0530

    smart table basic

--- src/app/pages/department/department.module.ts
+++ src/app/pages/department/department.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { AddDepartmentComponent } from './add-department/add-department.component';
 import { ViewDepartmentComponent } from './view-department/view-department.component';
 import { DepartmentRoutingModule } from './department-routing.module';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { NbCardModule, NbIconModule } from '@nebular/theme';
 
 
 @NgModule({
@@ -10,6 +12,9 @@ import { DepartmentRoutingModule } from './department-routing.module';
   imports: [
     CommonModule,
     DepartmentRoutingModule,
+    Ng2SmartTableModule,
+    NbCardModule,
+    NbIconModule,
   ]
 })
 export class DepartmentModule { }
