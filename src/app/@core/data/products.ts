dfad0ccdda30f7d1fcaffc6028b47beed0193eaa
nethmi-1997 <63659987+nethmi-1997@users.noreply.github.com>
2020-10-12 18:32:22 +0530

    nethmismart table part

---
+++ src/app/@core/data/products.ts
@@ -0,0 +1,21 @@
+import { Observable } from 'rxjs';
+
+
+export class Products{
+    id:number;
+    name: string;
+    reference_number:string;
+    quantity:string;
+    price:string;
+    type:string;
+    location:string;
+    weight:string;
+    description:string;
+}
+export abstract class ProductsData {
+     abstract getProducts():Observable<Products[]>;
+}
+
+
+
+
